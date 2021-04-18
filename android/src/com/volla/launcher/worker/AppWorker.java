94c33b44ec91090f4d1efdd0b88a4faad4b6f55a
Dr. Jörg Wurzer <github@online.de>
2021-04-18 17:40:29 +0200

    Release 1.2.6 fixed startup app id

--- android/src/com/volla/launcher/worker/AppWorker.java
+++ android/src/com/volla/launcher/worker/AppWorker.java
@@ -49,7 +49,7 @@ public class AppWorker
                                 "com.android.music", "com.android.inputmethod.latin", "com.android.stk", "com.mediatek.filemanager",
                                 "com.android.documentsui", "com.mediatek.cellbroadcastreceiver", "com.conena.navigation.gesture.control",
                                 "com.android.quicksearchbox", "com.android.dialer", "com.android.deskclock", "com.mediatek.gnss.nonframeworklbs",
-                                "com.volla.startup", "com.aurora.services");
+                                "system.volla.startup", "com.volla.startup", "com.aurora.services");
 
                             Intent i = new Intent(Intent.ACTION_MAIN, null);
                             i.addCategory(Intent.CATEGORY_LAUNCHER);
