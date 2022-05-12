bac2a0f35d019b8ff6205c60599d89bffdb7591c
Arvind0203 <arvind.yadav@tutanota.com>
2022-05-12 20:28:26 +0200

    Updating message Table columns

--- android/src/com/volla/launcher/storage/Message.java
+++ android/src/com/volla/launcher/storage/Message.java
@@ -2,9 +2,11 @@ package com.volla.launcher.storage;
 
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
+import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "messages")
+import com.volla.launcher.models.NotificationData;
+@Entity(tableName = "messages", indices = {@Index("title"), @Index("timeStamp")})
 public class Message {
 
     @ColumnInfo
@@ -17,13 +19,34 @@ public class Message {
     public String title;
 
     @ColumnInfo
+    public String address; // sender mobile number
+    @ColumnInfo
     public String selfDisplayName;
 
     @ColumnInfo
     public String largeIcon;
 
     @ColumnInfo
-    public String message;
+    public String text;
     @ColumnInfo
     public String notification;
+    @ColumnInfo
+    public Long timeStamp;
+    public NotificationData getNotificationData() {
+        return NotificationData.fromJson(notification);
+    }
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", uuid='" + uuid + '\'' +
+                ", title='" + title + '\'' +
+                ", selfDisplayName='" + selfDisplayName + '\'' +
+                ", address='" + address + '\'' +
+                ", largeIcon='" + largeIcon + '\'' +
+                ", text='" + text + '\'' +
+                ", notification='" + notification + '\'' +
+                ", timeStamp='" + timeStamp + '\'' +
+                '}';
+    }
 }
