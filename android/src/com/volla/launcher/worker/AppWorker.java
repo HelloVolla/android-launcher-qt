24d86a6952e69b6c79d56ac10ab9dfc2eaa6ee42
Dr. Jörg Wurzer <github@online.de>
2022-12-18 20:02:38 +0100

    Extending hidden apps

--- android/src/com/volla/launcher/worker/AppWorker.java
+++ android/src/com/volla/launcher/worker/AppWorker.java
@@ -55,7 +55,7 @@ public class AppWorker
                                 "com.android.gallery3d", "com.android.music", "com.android.inputmethod.latin", "com.android.stk",
                                 "com.mediatek.filemanager", "com.android.calendar", "com.android.documentsui", "com.google.android.gms",
                                 "com.mediatek.cellbroadcastreceiver", "com.conena.navigation.gesture.control",
-                                "com.android.quicksearchbox", "com.android.dialer", "com.android.deskclock",
+                                "com.android.quicksearchbox", "com.android.dialer", "com.android.deskclock", "com.pri.pressure",
                                 "com.mediatek.gnss.nonframeworklbs", "system.volla.startup", "com.volla.startup", "com.aurora.services",
                                 "com.android.soundrecorder", "com.google.android.dialer", "com.simplemobiletools.thankyou");
 
