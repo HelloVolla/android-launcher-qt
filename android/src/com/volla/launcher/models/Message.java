d4fc86cce916f0db3282ac11af9fd59f9e3aedc4
Arvind <arvind.yadav@tutanota.com>
2023-01-21 16:59:54 +0530

    Adding signal notification core code

---
+++ android/src/com/volla/launcher/models/Message.java
@@ -0,0 +1,17 @@
+package com.volla.launcher.models;
+
+public class Message {
+
+    public String id;
+
+    public String title; // receiver name
+
+    public String selfDisplayName; // sender name
+
+    public String largeIcon;
+
+    public String message;
+
+    public Notification notification;
+
+}
