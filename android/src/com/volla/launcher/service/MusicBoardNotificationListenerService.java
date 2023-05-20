99cdb9e85ffc53c667c90a82d7aeb52fb07f5574
Roker2 <dimaminko2000@gmail.com>
2023-05-20 22:41:21 +0200

    android: MusicBoardNotificationListenerService: Add track meta data

--- android/src/com/volla/launcher/service/MusicBoardNotificationListenerService.java
+++ android/src/com/volla/launcher/service/MusicBoardNotificationListenerService.java
@@ -3,6 +3,7 @@ package com.volla.launcher.service;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaMetadata;
 import android.media.session.MediaController;
 import android.media.session.MediaSessionManager;
 import android.os.IBinder;
@@ -40,6 +41,14 @@ public class MusicBoardNotificationListenerService extends NotificationListenerS
         List<MediaController> controllers = mediaSessionManager.getActiveSessions(componentName);
         for (MediaController controller : controllers) {
             Log.d(TAG, "current player: " + controller.getPackageName());
+            Intent intent = new Intent("com.volla.launcher.service");
+            MediaMetadata metadata = controller.getMetadata();
+            if (metadata != null) {
+                intent.putExtra("Music package", controller.getPackageName());
+                intent.putExtra("Song name", metadata.getString(MediaMetadata.METADATA_KEY_TITLE));
+                intent.putExtra("Song author", metadata.getString(MediaMetadata.METADATA_KEY_ARTIST));
+            }
+            sendBroadcast(intent);
         }
     }
 
