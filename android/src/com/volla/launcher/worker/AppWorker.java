7abd6671e8848193c1781abe37956e9cac9c9e28
Dr. Jörg Wurzer <github@online.de>
2024-05-12 19:09:54 +0200

    Fix for phone app

--- android/src/com/volla/launcher/worker/AppWorker.java
+++ android/src/com/volla/launcher/worker/AppWorker.java
@@ -55,11 +55,11 @@ public class AppWorker
                                 "com.android.gallery3d", "com.android.music", "com.android.inputmethod.latin", "com.android.stk",
                                 "com.mediatek.filemanager", "com.android.calendar", "com.android.documentsui", "com.google.android.gms",
                                 "com.mediatek.cellbroadcastreceiver", "com.conena.navigation.gesture.control", "rkr.simplekeyboard.inputmethod",
-                                "com.android.quicksearchbox", "com.android.dialer", "com.android.deskclock", "com.pri.pressure",
+                                "com.android.quicksearchbox", "org.fossify.phone", "com.android.deskclock", "com.pri.pressure",
                                 "com.mediatek.gnss.nonframeworklbs", "system.volla.startup", "com.volla.startup", "com.aurora.services",
                                 "com.android.soundrecorder", "com.google.android.dialer", "com.simplemobiletools.thankyou",
                                 "com.elishaazaria.sayboard", "com.jzhk.chlidmode", "com.jzhk.gamemode", "com.jzhk.tool",
-                                "com.google.android.apps.adm", "com.android.soundrecorder", "com.jzhk.easylauncher");
+                                "com.google.android.apps.adm", "com.android.soundrecorder", "com.jzhk.easylauncher", "com.simplemobiletools.dialer");
 
                             final List<String> mostUsed = Arrays.asList("com.android.dialer", "com.mediatek.camera",
                                 "com.simplemobiletools.dialer", "com.simplemobiletools.gallery.pro", "com.android.messaging",
